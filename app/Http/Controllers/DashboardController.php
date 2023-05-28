895a6f871d3bab877c4da1c3a56ad0c79d986c26
Eduardo Carlos <duureis@gmail.com>
2023-05-27 23:04:05 -0300

    wip

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Hit;
-use App\Models\Link;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -17,13 +16,13 @@ class DashboardController extends Controller
 
     public function __invoke()
     {
-        $data = [
+        $links_id = $this->request->user()->links->pluck('id');
+        return [
             'links' => $this->request->user()->links()->count(),
             'month_views' => $this->request->user()->links()->sum('views'),
-            'week_views' => Hit::query()->whereIn('link_id', $this->request->user()->links->pluck('id'))->whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])->count(),
-            'day_views' => Hit::query()->whereIn('link_id', $this->request->user()->links->pluck('id'))->whereBetween('created_at', [now()->startOfDay(), now()->endOfDay()])->count(),
-            'hour_views' => Hit::query()->whereIn('link_id', $this->request->user()->links->pluck('id'))->whereBetween('created_at', [now()->startOfHour(), now()->endOfHour()])->count(),
+            'week_views' => Hit::query()->whereIn('link_id', $links_id)->whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])->count(),
+            'day_views' => Hit::query()->whereIn('link_id', $links_id)->whereBetween('created_at', [now()->startOfDay(), now()->endOfDay()])->count(),
+            'hour_views' => Hit::query()->whereIn('link_id', $links_id)->whereBetween('created_at', [now()->startOfHour(), now()->endOfHour()])->count(),
         ];
-        return $data;
     }
 }
